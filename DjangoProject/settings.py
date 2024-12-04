f337d3767a0b859e52602e040dc2ae80abd0a2e0
Maksym Kashka <276488@student.pwr.edu.pl~>
2024-12-04 10:39:16 +0100

    List 6 Part 1

--- DjangoProject/settings.py
+++ DjangoProject/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'django_db',
         'USER': 'django',
-        'PASSWORD': '*******',
+        'PASSWORD': '********',
         'HOST': 'localhost',
         'PORT': '5432',
     }
